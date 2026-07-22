bf1a02e5aff4e43f2394e60a04b79ce34967bc70
yoch <yoch.melka@gmail.com>
2026-07-22 09:41:18 +0200

    Make July 22 visit plan the default

--- src/days/index.js
+++ src/days/index.js
@@ -1,10 +1,12 @@
 import day20260721 from "./2026-07-21.js";
+import day20260722 from "./2026-07-22.js";
 
 export const days = {
-  [day20260721.id]: day20260721
+  [day20260721.id]: day20260721,
+  [day20260722.id]: day20260722
 };
 
 export function getSelectedDay() {
-  const id = new URLSearchParams(location.search).get("day") || day20260721.id;
-  return days[id] || day20260721;
+  const id = new URLSearchParams(location.search).get("day") || day20260722.id;
+  return days[id] || day20260722;
 }
